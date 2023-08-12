a546b6ca31913b41d98ca3b5c8ba1ca47d5c5b26
Cozma Rares <cozmarares11@gmail.com>
2023-08-12 16:14:49 +0300

    chore: configure tailwind colors and fonts, and deploy script

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnightBlue: "#000330",
+        lavenderBlue: "#8E8EDB",
+        darkIndigo: "#29235D",
+        paleAqua: "#AAD9D9",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
